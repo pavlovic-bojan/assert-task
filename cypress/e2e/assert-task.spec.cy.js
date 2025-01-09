f87c1bb0dd693e836227cc918ad780aec9f292d1
Bojan Pavlovic <bojanpavlovicoffice@gmail.com>
2025-01-09 18:17:39 +0100

    add test case click on button

--- cypress/e2e/assert-task.spec.cy.js
+++ cypress/e2e/assert-task.spec.cy.js
@@ -12,20 +12,12 @@ describe('Task page for Assert QA', () => {
         cy.get(elements.H3OnPageLocator).should('have.text', elements.H3OnPageText)
     })
 
-
     it('Validate that the buttons are visible', () => {
         cy.get(elements.Button).should('be.visible')
     })
 
     it('Validate that the buttons edit & delete are visible', () => {
         cy.get(elements.EditDeleteButtonLocator).should('be.visible')
-    })
-
-    const buttons = [elements.Button, elements.EditDeleteButtonLocator]
-    buttons.forEach((button, i) => {
-        it.skip(`Validate that the buttons, and edit - delete are visible`, () => {
-            cy.get(button).should('be.visible')
-        })
     })
 
     it('Click on each edit button with text "Edit" and validate URL contains #edit', () => {
@@ -70,11 +62,35 @@ describe('Task page for Assert QA', () => {
             })
     })
 
-    it('Validate that the buttons are not visible - this test should always fail', () => {
-        cy.get(elements.Button).should('not.be.visible')
+    it('Validate clicks on each button with page reload after each click', () => {
+        cy.get(elements.button).then(($buttons) => {
+            const totalButtons = $buttons.length
+            for (let i = 0; i < totalButtons; i++) {
+                cy.visit(elements.FullPageUrl)
+                cy.get(elements.button).eq(i).click()
+                cy.get(elements.button).eq(i)
+                    .invoke('attr', 'class')
+                    .then((className) => {
+                        switch (i) {
+                            case 0:
+                                expect(className).to.contain(elements.button_1)
+                                break;
+                            case 1:
+                                expect(className).to.contain(elements.button_2)
+                                break;
+                            case 2:
+                                expect(className).to.contain(elements.button_3)
+                                break
+                            default:
+                                throw new Error(`Unexpected button index: ${i}`)
+                        }
+                    })
+            }
+        })
     })
 
-    it('Validate that the user is on the task page and perform lighthouse performance', () => {        cy.url().should('contains', elements.PageUrl)
+    it('Validate that the user is on the task page and perform lighthouse performance', () => {
+        cy.url().should('contains', elements.PageUrl)
         cy.title().should('eq', elements.PageTitle)
         cy.get(elements.H3OnPageLocator).should('have.text', elements.H3OnPageText)
         cy.lighthouse({
